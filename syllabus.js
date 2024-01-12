e9df0821ea14f151057eae793da834bbec3b033c
AliBaig102 <baiga8424@gmail.com>
2024-01-13 02:06:52 +0500

    this is a syllabus file for javascript

---
+++ syllabus.js
@@ -0,0 +1,29 @@
+// variables in javascript
+// primitives Data types in javascript
+// non-primitives Data types in javascript
+// arrays in javascript
+// objects in javascript
+// operators in javascript
+// loops in javascript
+// functions in javascript
+//conditionals in javascript
+// template string in javascript
+//oops in javascript
+// closure in javascript
+// asynchronous javascript
+// promises in javascript
+// classes in javascript
+// modules in javascript
+// document object model in javascript
+// browser object model in javascript
+// events in javascript
+// errors handling in javascript
+//fetch in javascript
+//storage in javascript
+//date and time in javascript
+//settimeout in javascript
+//setinterval in javascript
+//xhr in javascript
+//ajax in javascript
+//JSON in javascript
+// complete javascript tutorial
